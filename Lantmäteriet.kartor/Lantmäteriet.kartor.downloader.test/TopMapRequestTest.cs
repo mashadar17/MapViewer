d4e618a675da132658fb5d1235552bf37694a826
mashadar17 <pelf005@gmail.com>
2015-07-08 11:58:42 +0200

    added testproject
    added test for TopMapRequest

---
+++ Lantmäteriet.kartor/Lantmäteriet.kartor.downloader.test/TopMapRequestTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lantmäteriet.kartor.downloader.test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
